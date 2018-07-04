487d1cbb4bf64044a4c0111746e8079d2d5cbb9c
mganahl <mganahl@pitp.ca>
2018-07-04 16:17:09 -0400

    changed setup.py to include numpy

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
-setup(ext_modules=cythonize("./XXZED.pyx",language="c++"))
+import numpy
+setup(ext_modules=cythonize("./XXZED.pyx",language="c++"),include_dirs=[numpy.get_include()])
 #setup(ext_modules = cythonize("binarybasis.pyx",language="c++"))
